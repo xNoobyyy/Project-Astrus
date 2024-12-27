94a994a7e20becd821828bceb146753e39c80bfd
Leander Riefel <leanderriefel@gmx.de>
2024-12-27 23:51:31 +0100

    Item System start

---
+++ Assets/Scripts/Items/ItemSystem.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace Items {
+    public enum ItemType {
+        Combat,
+        Axe,
+        Pickaxe,
+        Resource,
+    }
+
+    public abstract class BaseItem : ScriptableObject {
+        public string itemName;
+        public ItemType itemType { get; protected set; }
+        public Sprite itemIcon;
+        public string itemDescription;
+    }
+
+    [CreateAssetMenu(fileName = "NewCombatItem", menuName = "Items/Combat")]
+    public class CombatItem : BaseItem {
+        private void OnEnable() {
+            itemType = ItemType.Combat;
+        }
+    }
+
+    [CreateAssetMenu(fileName = "NewAxeItem", menuName = "Items/Axe")]
+    public class AxeItem : BaseItem {
+        private void OnEnable() {
+            itemType = ItemType.Axe;
+        }
+    }
+
+    [CreateAssetMenu(fileName = "NewPickaxeItem", menuName = "Items/Pickaxe")]
+    public class PickaxeItem : BaseItem {
+        private void OnEnable() {
+            itemType = ItemType.Pickaxe;
+        }
+    }
+
+    [CreateAssetMenu(fileName = "NewResourceItem", menuName = "Items/Resource")]
+    public class ResourceItem : BaseItem {
+        private void OnEnable() {
+            itemType = ItemType.Resource;
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+namespace Player {
+    public class PlayerMovement : MonoBehaviour {
+        private CharacterController controller;
+
+        [SerializeField] private float speed = 1.5f;
+
+        private Vector3 movement;
+
+        private void Start() {
+            controller = GetComponent<CharacterController>();
+            if (controller != null) return;
+            Debug.LogError("CharacterController not found on Player");
+            enabled = false;
+        }
+
+        private void Update() {
+            var horizontalMove = Input.GetAxis("Horizontal");
+            var verticalMove = Input.GetAxis("Vertical");
+
+            movement = new Vector3(horizontalMove, verticalMove, 0);
+
+            if (movement.magnitude > 1) {
+                movement = movement.normalized;
+            }
+
+            movement *= speed;
+            controller.Move(movement * Time.deltaTime);
+        }
+    }
+}
