917e4524ae569d3d3d4d55071d0f29ba448bab38
Leander Riefel <leanderriefel@gmx.de>
2024-12-28 17:33:25 +0100

    Changed tilemap; Added stones as obstacles; Changed the player movement logic to use Rigidbody2D

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,32 +1,27 @@
 using UnityEngine;
 
 namespace Player {
+    [RequireComponent(typeof(Rigidbody2D))]
     public class PlayerMovement : MonoBehaviour {
-        private CharacterController controller;
+        private Rigidbody2D rb;
 
         [SerializeField] private float speed = 1.5f;
 
-        private Vector3 movement;
+        private Vector2 movement;
 
         private void Start() {
-            controller = GetComponent<CharacterController>();
-            if (controller != null) return;
-            Debug.LogError("CharacterController not found on Player");
-            enabled = false;
+            rb = GetComponent<Rigidbody2D>();
         }
 
         private void Update() {
-            var horizontalMove = Input.GetAxisRaw("Horizontal");
-            var verticalMove = Input.GetAxisRaw("Vertical");
+            movement.x = Input.GetAxisRaw("Horizontal");
+            movement.y = Input.GetAxisRaw("Vertical");
 
-            movement = new Vector3(horizontalMove, verticalMove, 0);
+            movement = movement.normalized;
+        }
 
-            if (movement.magnitude > 1) {
-                movement = movement.normalized;
-            }
-
-            movement *= speed;
-            controller.Move(movement * Time.deltaTime);
+        private void FixedUpdate() {
+            rb.linearVelocity = movement * speed;
         }
     }
 }
