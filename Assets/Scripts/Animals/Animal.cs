ed97a619c32daf7ac410c680bc7c79b130080c2e
Leander Riefel <leanderriefel@gmx.de>
2025-01-23 23:01:29 +0100

    quokka

---
+++ Assets/Scripts/Animals/Animal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Animals;
+using UnityEngine;
+
+namespace Pathfinding {
+    public class Animal : MonoBehaviour {
+        public Vector2 Target { get; private set; }
+        public Vector2 Position { get; private set; }
+        public float Health { get; private set; }
+
+        public float Health { get; private set; }
+
+        public AStar aStar;
+        public Collider2D collider;
+
+        public BaseAnimal baseAnimal;
+
+        private void Awake() {
+            aStar = new AStar(this);
+        }
+    }
+}
