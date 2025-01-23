eed4efaaa060d5cbe0e88dcc9686bf3c7c5be6ba
Leander Riefel <leanderriefel@gmx.de>
2025-01-23 23:32:10 +0100

    player animation

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
 
 namespace Player {
-    [RequireComponent(typeof(Rigidbody2D))]
+    [RequireComponent(typeof(Rigidbody2D), typeof(Animator))]
     public class PlayerMovement : MonoBehaviour {
+        private static readonly int HorizontalMove = Animator.StringToHash("horizontal_move");
+        private static readonly int VerticalMove = Animator.StringToHash("vertical_move");
         private Rigidbody2D rb;
+        private Animator animator;
 
         [SerializeField] private float speed = 1.5f;
 
@@ -11,6 +14,7 @@ namespace Player {
 
         private void Start() {
             rb = GetComponent<Rigidbody2D>();
+            animator = GetComponent<Animator>();
         }
 
         private void Update() {
@@ -18,6 +22,9 @@ namespace Player {
             movement.y = Input.GetAxisRaw("Vertical");
 
             movement = movement.normalized;
+
+            animator.SetFloat(HorizontalMove, movement.x);
+            animator.SetFloat(VerticalMove, movement.y);
         }
 
         private void FixedUpdate() {
