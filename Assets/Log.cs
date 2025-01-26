1cbfcd44e6832723942316fb94d5a60e96b3085e
daschyy19 <leodasch@web.de>
2025-01-26 22:26:08 +0100

    hover

---
+++ Assets/Log.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+public class Log : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Image image = gameObject.GetComponent<Image>();
+        
+        Color color = image.color; 
+        color.a = 0.5f;         
+        image.color = color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPointerEnter(PointerEventData eventData) {
+        gameObject.transform.localScale = new Vector2(4.5f, 4.5f);
+        
+        Image image = gameObject.GetComponent<Image>();
+        
+        Color color = image.color; 
+        color.a = 1f;         
+        image.color = color;
+
+    }
+
+    public void OnPointerExit(PointerEventData eventData) {
+        gameObject.transform.localScale = new Vector2(4, 4);
+        
+        Image image = gameObject.GetComponent<Image>();
+        
+        Color color = image.color; 
+        color.a = 0.5f;         
+        image.color = color;
+    }
+}
