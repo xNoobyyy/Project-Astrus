9b68d3c4906bd1c5fdff129e2aa924997629b0aa
Leander Riefel <leanderriefel@gmx.de>
2025-02-10 20:00:11 +0100

    swamp

---
+++ Assets/Prefabs/Ruletiles/FriendlyRuleTileEditor.cs
@@ -0,0 +1,30 @@
+#if UNITY_EDITOR
+using Prefabs.Ruletiles;
+using UnityEngine;
+
+namespace UnityEditor {
+    [CustomEditor(typeof(FriendlyRuleTile))]
+    [CanEditMultipleObjects]
+    public class AdvancedRuleTileEditor : RuleTileEditor {
+        public Texture2D anyIcon;
+        public Texture2D specifiedIcon;
+        public Texture2D nothingIcon;
+
+        public override void RuleOnGUI(Rect rect, Vector3Int position, int neighbor) {
+            switch (neighbor) {
+                case FriendlyRuleTile.Neighbor.Any:
+                    GUI.DrawTexture(rect, anyIcon);
+                    return;
+                case FriendlyRuleTile.Neighbor.Specified:
+                    GUI.DrawTexture(rect, specifiedIcon);
+                    return;
+                case FriendlyRuleTile.Neighbor.Nothing:
+                    GUI.DrawTexture(rect, nothingIcon);
+                    return;
+            }
+
+            base.RuleOnGUI(rect, position, neighbor);
+        }
+    }
+}
+#endif
