c10bbd29b2602ef8f92e875a05e1822702ffa7c4
xNoobyyy <riefel.leander@gmail.com>
2025-02-13 06:32:36 +0100

    audio

--- Assets/Scripts/Utils/Area.cs
+++ Assets/Scripts/Utils/Area.cs
@@ -1,18 +1,36 @@
+using System;
 using UnityEngine;
 
 namespace Utils {
     [RequireComponent(typeof(Collider2D))]
     public class Area : MonoBehaviour {
-        public enum AreaType {
-            Swamp,
-            Beach,
-            Grasslands,
-            Plateau,
-            City,
-            Starter
-        }
-
         public AreaType type;
         public AudioClip[] music;
+
+        private void OnTriggerEnter2D(Collider2D other) {
+            if (!other.CompareTag("Player")) return;
+
+            PlayRandomMusic();
+        }
+
+        private void OnTriggerExit2D(Collider2D other) {
+            if (!other.CompareTag("Player")) return;
+
+            AudioManager.Instance.StopMusic();
+        }
+
+        private void PlayRandomMusic() {
+            AudioManager.Instance.PlayMusic(music[UnityEngine.Random.Range(0, music.Length)],
+                onMusicEnd: PlayRandomMusic);
+        }
+    }
+
+    public enum AreaType {
+        Swamp,
+        Beach,
+        Grasslands,
+        Plateau,
+        City,
+        Starter
     }
 }
---
+++ Assets/Scripts/Utils/AudioManager.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+namespace Utils {
+    using UnityEngine;
+
+    public class AudioManager : MonoBehaviour {
+        public static AudioManager Instance;
+        private AudioSource audioSource;
+        private Action onMusicEndCallback;
+        private Coroutine fadeOutCoroutine;
+
+        private void Awake() {
+            if (Instance == null) {
+                Instance = this;
+            } else {
+                Destroy(gameObject);
+            }
+
+            audioSource = GetComponent<AudioSource>();
+        }
+
+        public void PlayMusic(AudioClip clip, float volume = 1.0f, Action onMusicEnd = null) {
+            if (audioSource.clip == clip) return;
+            audioSource.clip = clip;
+            audioSource.volume = volume;
+            audioSource.Play();
+
+            onMusicEndCallback = onMusicEnd;
+            StartCoroutine(CheckMusicEnd());
+        }
+
+        private IEnumerator CheckMusicEnd() {
+            yield return new WaitUntil(() => !audioSource.isPlaying);
+            yield return new WaitForSeconds(5);
+
+            onMusicEndCallback?.Invoke();
+        }
+
+        public void StopMusic() {
+            StopCoroutine(CheckMusicEnd());
+            onMusicEndCallback = null;
+
+            if (fadeOutCoroutine != null) StopCoroutine(fadeOutCoroutine);
+            fadeOutCoroutine = StartCoroutine(FadeOut());
+
+            audioSource.Stop();
+        }
+
+        private IEnumerator FadeOut(float fadeDuration = 4.0f) {
+            var startVolume = audioSource.volume;
+
+            for (float t = 0; t < fadeDuration; t += Time.deltaTime) {
+                audioSource.volume = Mathf.Lerp(startVolume, 0, t / fadeDuration);
+                yield return null;
+            }
+
+            audioSource.volume = 0;
+            audioSource.Stop();
+        }
+
+        public void SetVolume(float volume) {
+            audioSource.volume = volume;
+        }
+    }
+}
