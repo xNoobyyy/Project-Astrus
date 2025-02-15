2d8c15e8c6bc41ca7e439fa3076646b619d7b734
Leander Riefel <leanderriefel@gmx.de>
2025-02-15 20:22:45 +0100

    VOLUMESSSSSSS

--- Assets/Scripts/Utils/Area.cs
+++ Assets/Scripts/Utils/Area.cs
@@ -1,22 +1,67 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 namespace Utils {
     [RequireComponent(typeof(Collider2D))]
     public class Area : MonoBehaviour {
+        private const float FadeDuration = 2.0f;
+
         public AreaType type;
         public AudioClip[] music;
+
+        private Coroutine canvasFadeCoroutine;
+
+        private readonly Dictionary<Volume, Coroutine> volumeFadeCoroutines = new Dictionary<Volume, Coroutine>();
 
         private void OnTriggerEnter2D(Collider2D other) {
             if (!other.CompareTag("Player")) return;
 
             PlayRandomMusic();
+
+            AreaManager.Instance.fogCanvas.enabled = type is AreaType.Swamp or AreaType.City;
+
+            FadeVolumeTo(AreaManager.Instance.swampVolume, (type is AreaType.Swamp) ? 1f : 0f, FadeDuration);
+            FadeVolumeTo(AreaManager.Instance.popVolume,
+                (type is not (AreaType.Swamp or AreaType.City or AreaType.Jungle)) ? 1f : 0f, FadeDuration);
+            FadeVolumeTo(AreaManager.Instance.jungleVolume, (type is AreaType.Jungle) ? 1f : 0f, FadeDuration);
+            FadeVolumeTo(AreaManager.Instance.cityVolume, (type is AreaType.City) ? 1f : 0f, FadeDuration);
         }
 
         private void OnTriggerExit2D(Collider2D other) {
             if (!other.CompareTag("Player")) return;
 
             AudioManager.Instance.StopMusic();
+
+            AreaManager.Instance.fogCanvas.enabled = false;
+
+            FadeVolumeTo(AreaManager.Instance.swampVolume, 0f, FadeDuration);
+            FadeVolumeTo(AreaManager.Instance.popVolume, 0f, FadeDuration);
+            FadeVolumeTo(AreaManager.Instance.jungleVolume, 0f, FadeDuration);
+            FadeVolumeTo(AreaManager.Instance.cityVolume, 0f, FadeDuration);
+        }
+
+        private static IEnumerator FadeVolume(Volume volume, float targetWeight, float duration) {
+            var startWeight = volume.weight;
+            var elapsed = 0f;
+            while (elapsed < duration) {
+                volume.weight = Mathf.Lerp(startWeight, targetWeight, elapsed / duration);
+                elapsed += Time.deltaTime;
+                yield return null;
+            }
+
+            volume.weight = targetWeight;
+        }
+
+        private void FadeVolumeTo(Volume volume, float targetWeight, float duration) {
+            if (volumeFadeCoroutines.TryGetValue(volume, out var existing)) {
+                StopCoroutine(existing);
+            }
+
+            var newCoroutine = StartCoroutine(FadeVolume(volume, targetWeight, duration));
+            volumeFadeCoroutines[volume] = newCoroutine;
         }
 
         private void PlayRandomMusic() {
---
+++ Assets/Scripts/Utils/AreaManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace Utils {
+    public class AreaManager : MonoBehaviour {
+        public static AreaManager Instance { get; private set; }
+
+        public Volume popVolume;
+        public Volume swampVolume;
+        public Canvas fogCanvas;
+        public Volume jungleVolume;
+        public Volume cityVolume;
+        
+        private void Awake() {
+            if (Instance == null) {
+                Instance = this;
+            } else {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
