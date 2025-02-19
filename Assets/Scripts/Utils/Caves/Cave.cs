2b9512a20efcefd75b0b847d534f0efe2861a20a
Leander Riefel <leanderriefel@gmx.de>
2025-02-19 20:07:35 +0100

    first cave stuff will add transition later

---
+++ Assets/Scripts/Utils/Caves/Cave.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Utils.Caves {
+    public class Cave : MonoBehaviour {
+        private void OnTriggerEnter2D(Collider2D other) {
+            if (other.CompareTag("Player")) {
+                CaveManager.Instance.ToggleCave();
+            }
+        }
+    }
+}
