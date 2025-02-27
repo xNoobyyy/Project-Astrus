2cd226e9e39a05ee6ea41f64a7b5837fcb82b5a3
xNoobyyy <riefel.leander@gmail.com>
2025-02-27 12:50:47 +0100

    some bow logic changes idfk what i did tho

--- Assets/Scripts/Items/BowItem.cs
+++ Assets/Scripts/Items/BowItem.cs
@@ -1,15 +1,29 @@
+using System;
+using System.Linq;
+using Creatures;
 using UnityEditor.Animations;
 using UnityEngine;
 
 namespace Items {
     public abstract class BowItem : Item {
+        private const float MAX_DISTANCE = 100f;
+
         public int Damage { get; private set; }
 
-        protected BowItem(string name, string description, Sprite icon, AnimatorController animator, int damage) : base(name, description, icon, animator) {
+        protected BowItem(string name, string description, Sprite icon, AnimatorController animator, int damage) : base(
+            name, description, icon, animator) {
             Damage = damage;
         }
 
         public override void OnUse(Transform player, Vector3 position) {
+            var v = (Vector2)(position - player.position);
+            var hits = Physics2D.RaycastAll(player.position, v, MAX_DISTANCE);
+            Array.Sort(hits, (a, b) => a.distance.CompareTo(b.distance));
+            hits.Select(hit => hit.collider.GetComponent<CreatureBase>())
+                .FirstOrDefault(creature => creature != null)?
+                .GetComponent<CreatureBase>()?
+                .OnAttack(player, Damage);
+
             OnAttack();
         }
 
