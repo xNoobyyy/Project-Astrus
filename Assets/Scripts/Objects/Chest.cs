44d4c6ccf0dff7edab353a7ec59c1af34039624f
xNoobyyy <riefel.leander@gmail.com>
2025-02-28 12:36:53 +0100

    chest

---
+++ Assets/Scripts/Objects/Chest.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using Items;
+using Items.Items;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+namespace Objects {
+    public class Chest : MonoBehaviour {
+        private const float RespawnTime = 600000; // 10 minutes
+
+        [SerializeField] private Sprite closed;
+        [SerializeField] private Sprite open;
+        [SerializeField] public Collider2D trigger;
+        [SerializeField] private new Light2D light;
+
+        private SpriteRenderer spriteRenderer;
+        private Coroutine respawnCoroutine;
+
+        public long OpenedAt { get; private set; }
+        public bool IsOpen => OpenedAt != -1;
+
+        private void Awake() {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+
+            OpenedAt = -1;
+        }
+
+        private void OnEnable() {
+            if (!IsOpen) return;
+            if (respawnCoroutine != null) {
+                StopCoroutine(respawnCoroutine);
+                respawnCoroutine = null;
+            }
+
+            respawnCoroutine = StartCoroutine(RespawnTimer());
+        }
+
+        private void OnDisable() {
+            if (respawnCoroutine == null) return;
+            StopCoroutine(respawnCoroutine);
+            respawnCoroutine = null;
+        }
+
+        private IEnumerator RespawnTimer() {
+            var currentTime = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+            var timeLeft = RespawnTime - (currentTime - OpenedAt);
+
+            if (timeLeft > 0) {
+                yield return new WaitForSeconds(timeLeft / 1000f);
+            }
+
+            Respawn();
+        }
+
+        public void Open() {
+            spriteRenderer.sprite = open;
+            light.enabled = false;
+
+            OpenedAt = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+            ItemManager.Instance.DropItem(new Domilitant(1), transform.position);
+            ItemManager.Instance.DropItem(new Domilitant(1), transform.position);
+
+            spriteRenderer.sprite = open;
+
+            StartCoroutine(RespawnTimer());
+        }
+
+        public void Respawn() {
+            if (respawnCoroutine != null) {
+                StopCoroutine(respawnCoroutine);
+                respawnCoroutine = null;
+            }
+
+            spriteRenderer.sprite = closed;
+            light.enabled = true;
+
+            OpenedAt = -1;
+        }
+
+        private void OnValidate() {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+            spriteRenderer.sprite = closed;
+        }
+    }
+}
